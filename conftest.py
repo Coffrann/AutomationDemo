a3554d288753625f27ff73687791bb33e2266ac8
Coffrann <franco.kleinerman@gmail.com>
2024-05-31 21:51:54 -0300

    Added support for headless argument

--- conftest.py
+++ conftest.py
@@ -2,10 +2,20 @@ import pytest
 from playwright.sync_api import sync_playwright
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless",
+        action="store_true",
+        default=False,
+        help="Run playwright tests in headless mode"
+    )
+
+
 @pytest.fixture(scope="session")
-def browser():
+def browser(pytestconfig):
+    headless_arg = pytestconfig.getoption("--headless")
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=headless_arg)
         yield browser
         browser.close()
 
